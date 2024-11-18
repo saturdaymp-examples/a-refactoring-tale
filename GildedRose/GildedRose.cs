09e6a0a4c7075efd2b52b6a5d2b83dc08287f854
Chris C <chris.cumming@saturdaymp.com>
2024-11-18 16:28:10 -0700

    Extract UpdateItem method.
    Separated item update logic into its own method to improve code readability and maintainability.

--- GildedRose/GildedRose.cs
+++ GildedRose/GildedRose.cs
@@ -12,14 +12,71 @@ public class GildedRose(IList<Item> items)
     {
         foreach (var item in items)
         {
-            if (item.Name != AgedBrie && item.Name != BackstagePassesATafkal80Etc)
+            UpdateItem(item);
+        }
+    }
+
+    private static void UpdateItem(Item item)
+    {
+        if (item.Name != AgedBrie && item.Name != BackstagePassesATafkal80Etc)
+        {
+            if (item.Quality > 0)
             {
-                if (item.Quality > 0)
+                if (item.Name != SulfurasHandOfRagnaros)
                 {
-                    if (item.Name != SulfurasHandOfRagnaros)
+                    item.Quality -= 1;
+                }
+            }
+        }
+        else
+        {
+            if (item.Quality < 50)
+            {
+                item.Quality += 1;
+
+                if (item.Name == BackstagePassesATafkal80Etc)
+                {
+                    if (item.SellIn < 11)
                     {
-                        item.Quality -= 1;
+                        if (item.Quality < 50)
+                        {
+                            item.Quality += 1;
+                        }
                     }
+
+                    if (item.SellIn < 6)
+                    {
+                        if (item.Quality < 50)
+                        {
+                            item.Quality += 1;
+                        }
+                    }
+                }
+            }
+        }
+
+        if (item.Name != SulfurasHandOfRagnaros)
+        {
+            item.SellIn -= 1;
+        }
+
+        if (item.SellIn < 0)
+        {
+            if (item.Name != AgedBrie)
+            {
+                if (item.Name != BackstagePassesATafkal80Etc)
+                {
+                    if (item.Quality > 0)
+                    {
+                        if (item.Name != SulfurasHandOfRagnaros)
+                        {
+                            item.Quality -= 1;
+                        }
+                    }
+                }
+                else
+                {
+                    item.Quality -= item.Quality;
                 }
             }
             else
@@ -27,58 +84,6 @@ public class GildedRose(IList<Item> items)
                 if (item.Quality < 50)
                 {
                     item.Quality += 1;
-
-                    if (item.Name == BackstagePassesATafkal80Etc)
-                    {
-                        if (item.SellIn < 11)
-                        {
-                            if (item.Quality < 50)
-                            {
-                                item.Quality += 1;
-                            }
-                        }
-
-                        if (item.SellIn < 6)
-                        {
-                            if (item.Quality < 50)
-                            {
-                                item.Quality += 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (item.Name != SulfurasHandOfRagnaros)
-            {
-                item.SellIn -= 1;
-            }
-
-            if (item.SellIn < 0)
-            {
-                if (item.Name != AgedBrie)
-                {
-                    if (item.Name != BackstagePassesATafkal80Etc)
-                    {
-                        if (item.Quality > 0)
-                        {
-                            if (item.Name != SulfurasHandOfRagnaros)
-                            {
-                                item.Quality -= 1;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        item.Quality -= item.Quality;
-                    }
-                }
-                else
-                {
-                    if (item.Quality < 50)
-                    {
-                        item.Quality += 1;
-                    }
                 }
             }
         }
