34e80d7352b4d02b0410ba348366ec4b31b7d904
Chris C <chris.cumming@saturdaymp.com>
2024-11-20 12:40:13 -0700

    Refactor Conjured item quality update logic

--- GildedRose/EndOfDayCalculator.cs
+++ GildedRose/EndOfDayCalculator.cs
@@ -90,22 +90,11 @@ public static class EndOfDayCalculator
             }
         }
     }
-
+    
     private static void UpdateConjured(Item item)
     {
         item.SellIn -= 1;
-        if (item.SellIn >= 0)
-        {
-            item.Quality -= 2;
-        }
-        else
-        {
-            item.Quality -= 4;
-        }
-
-        if (item.Quality < 0)
-        {
-            item.Quality = 0;
-        }
+        item.Quality -= item.SellIn >= 0 ? 2 : 4;
+        if (item.Quality < 0) item.Quality = 0;
     }
 }
